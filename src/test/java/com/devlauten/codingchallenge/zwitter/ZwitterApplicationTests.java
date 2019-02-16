a2efcf2c9cc99dd0937573f1f501f92c57e1fc07
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2019-02-16 19:07:19 +0100

    Initial project template

---
+++ src/test/java/com/devlauten/codingchallenge/zwitter/ZwitterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devlauten.codingchallenge.zwitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZwitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
